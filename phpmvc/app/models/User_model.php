10cbb59c70bbfcbe6ba2825f2ad0c451d4f1bed6
MuhammadSofi07 <61622460+MuhammadSofi07@users.noreply.github.com>
2021-06-06 00:53:25 +0700

    Add files via upload

---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Sofi, Iqbal';
+
+    public function getuser()
+    {
+        return $this->nama;
+    }
+}
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+
+    <h1 class="mt-5">About Us</h1>
+    <img src="<?= BASEURL; ?>/img/nba2k21.jpg" alt="Tugas Pemro Web" width="200" class="rounded-circle shadow">
+    <p>Halo, Kami <?= $data['nama']; ?>, Pekerjaan Kami <?= $data['pekerjaan']; ?>, Umur <?= $data['umur']; ?> Kelas PWEB D </p>
+
+</div>
